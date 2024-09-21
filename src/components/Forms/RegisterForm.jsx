816a4674286f54456b0b89a3889b6753948162ee
Rahaf Saud <rahafsaud459@gmail.com>
2024-09-21 17:56:19 +0300

    v2

--- src/components/Forms/RegisterForm.jsx
+++ src/components/Forms/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Group, TextInput } from '@mantine/core';
+import { Box, Button, Checkbox, Group, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { IconUser, IconPhone, IconMail, IconPassword, IconCodeCircle, } from '@tabler/icons-react';
 import '../../assets/css/register/register.css'
@@ -22,33 +22,34 @@ export const RegisterForm = ({ name, phone, code, password, email }) => {
     return (
         <form onSubmit={form.onSubmit((values) => console.log(values))} className='form'>
 
-            <TextInput
-                className='registerform'
-                placeholder="ادخل اسمك الكامل"
-                rightSection={<IconUser style={{ marginRight: 10 }} />}
-            />
-            <TextInput
-                className='registerform'
-                placeholder="ادخل رقم الهاتف"
-                rightSection={<IconPhone style={{ marginRight: 10 }} />}
-            />
-            <TextInput
-                className='registerform'
-                placeholder="ادخل كود التسجيل"
-                rightSection={<IconCodeCircle style={{ marginRight: 10 }} />}
-            />
-            <TextInput
-            className='registerform'
-                placeholder="ادخل البريد الإلكتروني"
-                rightSection={<IconMail style={{ marginRight: 10 }} />}
-            />
-            <TextInput
-            className='registerform'
-                placeholder="ادخل كلمة المرور"
-                rightSection={<IconPassword style={{ marginRight: 10 }} />}
-            />
+            <Box className='box-form'>
+                <TextInput
+                    className='registerform'
+                    placeholder="ادخل اسمك الكامل"
+                    rightSection={<IconUser style={{ marginRight: 10 }} />}
+                />
+                <TextInput
+                    className='registerform'
+                    placeholder="ادخل رقم الهاتف"
+                    rightSection={<IconPhone style={{ marginRight: 10 }} />}
+                />
+                <TextInput
+                    className='registerform'
+                    placeholder="ادخل كود التسجيل"
+                    rightSection={<IconCodeCircle style={{ marginRight: 10 }} />}
+                />
+                <TextInput
+                    className='registerform'
+                    placeholder="ادخل البريد الإلكتروني"
+                    rightSection={<IconMail style={{ marginRight: 10 }} />}
+                />
+                <TextInput
+                    className='registerform'
+                    placeholder="ادخل كلمة المرور"
+                    rightSection={<IconPassword style={{ marginRight: 10 }} />}
+                />
 
-
+            </Box>
             <Checkbox
                 mt="md"
                 label="اوافق على شروط الخصوصية"
