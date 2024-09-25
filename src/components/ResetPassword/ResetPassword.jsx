dd7eace2f68ce53499bdf4a9d1343bc652b43113
Rahaf Saud <rahafsaud459@gmail.com>
2024-09-25 17:10:36 +0300

    v2

--- src/components/ResetPassword/ResetPassword.jsx
+++ src/components/ResetPassword/ResetPassword.jsx
@@ -12,59 +12,54 @@ export const ResetPassword = ({ email, newPassword }) => {
   const [opened, setOpened] = useState(false);
   return (
     <>
-      <div
-        style={{
-          fontSize: "12px",
-          textDecoration: "underline",
-          cursor: "pointer",
-          marginRight: "50px",
-          // margin:"auto",
-          direction: "rtl",
-        }}
-      >
+      <div>
         <p
           onClick={() => setOpened(true)}
           style={{
             cursor: "pointer",
             textDecoration: "underline",
+            textAlign:"right",
+            marginRight:40
           }}
         >
           هل نسيت كلمة المرور؟
         </p>
-<div  >
-        <Modal dir="rtl"
-          opened={opened}
-          onClose={() => setOpened(false)}
-          title="إعادة تعيين كلمة المرور"
-        >
-          <Paper radius="md" p="xl" withBorder>
-            <form>
-              <TextInput dir="rtl"
-                label="البريد الإلكتروني"
-                placeholder="أدخل بريدك الإلكتروني"
-                value={email}
-                required
-                mb="md"
-              />
-              <PasswordInput
-                label="كلمة المرور الجديدة"
-                placeholder="أدخل كلمة المرور الجديدة"
-                value={newPassword}
-                required
-                mb="md"
-              />
-              <PasswordInput
-                label="تأكيد كلمة المرور الجديدة"
-                placeholder="أدخل تأكيد كلمة المرور"
-                required
-                mb="md"
-              />
-              <Button type="submit" fullWidth mt="xl">
-                إعادة تعيين كلمة المرور
-              </Button>
-            </form>
-          </Paper>
-        </Modal>
+        <div>
+          <Modal
+            dir="rtl"
+            opened={opened}
+            onClose={() => setOpened(false)}
+            title="إعادة تعيين كلمة المرور"
+          >
+            <Paper radius="md" p="xl" withBorder>
+              <form>
+                <TextInput
+                  dir="rtl"
+                  label="البريد الإلكتروني"
+                  placeholder="أدخل بريدك الإلكتروني"
+                  value={email}
+                  required
+                  mb="md"
+                />
+                <PasswordInput
+                  label="كلمة المرور الجديدة"
+                  placeholder="أدخل كلمة المرور الجديدة"
+                  value={newPassword}
+                  required
+                  mb="md"
+                />
+                <PasswordInput
+                  label="تأكيد كلمة المرور الجديدة"
+                  placeholder="أدخل تأكيد كلمة المرور"
+                  required
+                  mb="md"
+                />
+                <Button type="submit" fullWidth mt="xl" color="red">
+                  إعادة تعيين كلمة المرور
+                </Button>
+              </form>
+            </Paper>
+          </Modal>
         </div>
       </div>
     </>
